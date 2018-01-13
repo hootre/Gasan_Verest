2e40c67f37ecd5dad82d8e358b66360632a1ce25
DONG <zxv7295@naver.com>
2018-01-13 17:48:44 +0900

    불필요한 데이터 삭제, 후기 service생성

---
+++ Gasan_Verest/src/main/java/com/verest/board/service/backsightService.java
@@ -0,0 +1,28 @@
+package com.verest.board.service;
+
+import java.util.List;
+
+import com.verest.board.model.Backsight;
+import com.verest.board.model.CommonException;
+import com.verest.board.model.Port;
+
+public interface backsightService {
+
+		// 게시물 번호를 1 증가하고 게시물을 등록한다.
+		public void newBoard(Backsight backsight) throws CommonException;
+
+		// 게시물 하나를 가져온다.
+		public Backsight detail(String back_no) throws CommonException;
+
+		// 게시물 전체를 가져온다.
+		public List<Backsight> list() throws CommonException;
+
+		// 게시물의 개수를 가져온다.
+		public int count() throws CommonException;
+
+		// 게시물을 수정한다. (수정되기 전 파일명을 리턴)
+		public void modify(Backsight backsight) throws CommonException;
+
+		// 게시물을 삭제한다. (삭제하는 파일명을 리턴)
+		public void remove(String back_no) throws CommonException;
+}
---
+++ Gasan_Verest/src/main/java/com/verest/board/service/backsightServiceImpl.java
@@ -0,0 +1,48 @@
+package com.verest.board.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.verest.board.dao.BacksightDao;
+import com.verest.board.model.Backsight;
+import com.verest.board.model.CommonException;
+
+@Service
+public class backsightServiceImpl implements backsightService{
+
+	@Autowired
+	private BacksightDao backDao;
+
+	@Override
+	public void newBoard(Backsight backsight) throws CommonException {
+		backDao.insert(backsight);
+		
+	}
+
+	@Override
+	public Backsight detail(String back_no) throws CommonException {
+		return  backDao.select(back_no);
+	}
+
+	@Override
+	public List<Backsight> list() throws CommonException {
+		return backDao.selectAll();
+	}
+
+	@Override
+	public int count() throws CommonException {
+		return backDao.backsightCount();
+	}
+
+	@Override
+	public void modify(Backsight backsight) throws CommonException {
+		 backDao.update(backsight);
+	}
+
+	@Override
+	public void remove(String back_no) throws CommonException {
+		backDao.delete(back_no);
+	}
+}

52cb6eb1a102233676491a31a4fe21b8feeebe68
fetra09 <fetra09@naver.com>
2018-01-18 21:21:26 +0900

    detail추가

--- Gasan_Verest/src/main/java/com/verest/board/controller/UserWebController.java
+++ Gasan_Verest/src/main/java/com/verest/board/controller/UserWebController.java
@@ -1,5 +1,6 @@
 package com.verest.board.controller;
 
+import java.net.URLDecoder;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.verest.board.model.UserInfo;
 import com.verest.board.model.CommonException;
@@ -62,6 +64,26 @@ public class UserWebController {
 
 		return "list";
 	}
+	
+	// 글 상세 화면
+		@RequestMapping(value = "/detail", method = RequestMethod.GET)
+		public String detail(Model model, 
+				@RequestParam(value = "no", required=true) String no)
+						throws CommonException, Exception {
+			Port port = null;
+			String filename = null;
+
+			port = portService.detail(no);
+			filename = port.getAttachment();
+			if (filename != null && !filename.trim().isEmpty()) {
+				filename = URLDecoder.decode(filename, "UTF-8");
+			}
+
+			model.addAttribute("item", port);
+			model.addAttribute("filename", filename);
+
+			return "detail";	// /WEB-INF/views/detail.jsp 페이지로 이동
+		}
 
 	// 현재 접속한 사용자의 email 리턴
 	private String getPrincipal() {
